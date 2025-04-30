327589795c22f75c867e1284ee44e2d340de5d09
Nicholas Weaver <ncweaver@ucdavis.edu>
2025-04-30 19:31:13 +0000

    Ok, something is weird, I'm seeing negative speedup on parallelization, like the compiler is way way smarter in single-process mode...

---
+++ matrix.hpp
@@ -0,0 +1,144 @@
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+#include <random>
+#include <omp.h>
+
+
+
+// Declaration before use
+template <class T> class Matrix;
+template <class T>
+Matrix<T> parallel_multiply(Matrix<T> &a, Matrix<T> &b);
+
+
+// This is a container for single precision square matrices in row-major form
+// We implement access as a () operator rather than a [] operator because
+// we can't have , in bracket operator and we want to be able to change the 
+// representation in variants.
+template <class T>
+class Matrix {
+public:
+
+    Matrix(size_t size, bool random = false) {
+        _data = new T[size * size];
+        _size = size;
+        if(random) {randomize();}
+    }
+
+    Matrix(Matrix &m){
+        _size = m._size;
+        _data = new T[_size * _size];
+        for(size_t i = 0; i < (_size * _size); ++i){
+            _data[i] = m._data[i];
+        }
+    }
+
+    Matrix(Matrix &&m){
+        _size = m._size;
+        _data = m._data;
+        m._data = nullptr;
+        m._size = 0;
+    }
+
+    Matrix<T> &operator=(Matrix &m){
+        if (&m == this) return *this;
+        if (_size > 0) delete _data;
+        _data = new T[_size * _size];
+        for(size_t i = 0; i < (_size * _size); ++i){
+            _data[i] = m._data[i];
+        }
+        return *this;
+    }
+
+    Matrix<T> &operator=(Matrix &&m){
+        _size = m._size;
+        _data = m._data;
+        m._data = nullptr;
+        m._size = 0;
+        return *this;
+    }
+
+    ~Matrix(){
+        if(_data) delete _data;
+    }
+
+    void randomize() {
+        std::random_device rd;  // Will be used to obtain a seed for the random number engine
+        std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+        std::uniform_real_distribution<> dis(1.0, 2.0);
+        for(size_t i = 0; i < (_size * _size); ++i){
+            _data[i] = (T) dis(gen);
+        }
+    
+    }
+
+    T& operator() (size_t row, size_t column) noexcept {
+        assert(row < _size);
+        assert(column < _size);
+        return (_data[row * _size + column]);
+    }
+
+    Matrix<T> operator*(Matrix &b) {
+        assert(_size == b._size);
+        Matrix<T> dest(_size);
+        for(size_t i = 0; i < _size; ++i){
+            for(size_t j = 0; j < _size; ++j){
+                T tmp = 0;
+                for(size_t k = 0; k < _size; ++k){
+                    tmp += (*this)(i,k) * b(k,j);
+                }
+                dest(i,j) = tmp;
+            }
+        }
+        return dest;
+    }
+
+    T *_data;
+    size_t _size;
+};
+
+template <class T>
+Matrix<T> parallel_multiply(Matrix<T> &a, Matrix<T> &b){
+    assert(a._size == b._size);
+    Matrix<T> dest(a._size);
+    auto size = a._size;
+#pragma omp parallel for
+    for(size_t i = 0; i < size; ++i){
+        for(size_t j = 0; j < size; ++j){
+            T tmp = 0;
+            for(size_t k = 0; k < size; ++k){
+                tmp += a(i,k) * b(k,j);
+            }
+            dest(i,j) = tmp;
+        }
+    }
+    return dest;
+}
+
+template <class T>
+Matrix<T> parallel_multiply2(Matrix<T> &a, Matrix<T> &b){
+    assert(a._size == b._size);
+    Matrix<T> dest(a._size);
+    auto size = a._size;
+    auto dataa = a._data;
+    auto datab = b._data;
+    auto datad = dest._data;
+#pragma omp parallel for
+    for(size_t i = 0; i < size; ++i){
+        auto isize = i * size;
+        for(size_t j = 0; j < size; ++j){
+            T tmp = 0;
+            auto jsize = j * size;
+            for(size_t k = 0; k < size; ++k){
+                tmp += dataa[isize + k] * datab[k * size + j];
+                // a(i,k) * b(k,j);
+            }
+            datad[isize + j] = tmp;
+        }
+    }
+    return dest;
+}
+
+
+#endif
--- parallel_tests.cpp
+++ parallel_tests.cpp
@@ -3,6 +3,7 @@
 #include <omp.h>
 #include "parallel_utils.hpp"
 #include <chrono>
+#include "matrix.hpp"
 
 // Demonstrate some basic assertions.
 TEST(ParallelTest, HelloWorld)
@@ -31,5 +32,67 @@ TEST(ParallelTest, CacheNuke)
     });
 
     std::cout << "Difference 2" << big << "\n";
-
 }
+
+TEST(ParallelTest, SimpleMatrix){
+    Matrix<float> a(2);
+    Matrix<float> b(2, true);
+    a(0,0)  = 1;
+    a(0,1) = 2;
+    a(1,0) = 1;
+    a(1,1) = 2;
+    b(0,0) = 1;
+    b(0,1) = 1;
+    b(1,0) = 1;
+    b(1,1) = 1;
+    auto d = a * b;
+
+    EXPECT_EQ(a(0,0), 1);
+    EXPECT_EQ(a(0,1), 2);
+    EXPECT_EQ(d(0,0), 3);
+    auto d2 = parallel_multiply(a, b);
+    EXPECT_EQ(d2(0,0), 3);
+}
+
+void parallelNuke(){
+#pragma omp parallel 
+{
+    CacheNuke();
+}
+}
+
+
+TEST(ParallelTest, ParallelSpeedup) {
+    size_t s = 16;
+    // Enough to nuke L1 but not L2/L3
+    // But critically leaves both runs in basically the 
+    // same position
+    CacheNukePrepare(64*1024);
+    for(size_t i = 0; i < 6; ++i) {
+        s = s << 1;
+        std::cout << "Timing Matrix size " << s << "\n";
+        Matrix<float> a(s, true);
+        Matrix<float> b(s, true);
+        // parallelNuke();
+        auto sequential = GetTiming([&]() {
+            auto d = a * b;
+            (void) d;
+        });
+        // parallelNuke();
+        auto parallel = GetTiming([&]() {
+            auto d = parallel_multiply(a, b);
+            (void) d;
+        });
+
+        auto parallel2 = GetTiming([&]() {
+            auto d = parallel_multiply2(a, b);
+            (void) d;
+        });
+        std::cout << "Sequential timing: " << sequential << "\n";
+        std::cout << "Parallel timing:   " << parallel << "\n";
+        std::cout << "Parallel timing2:  " << parallel2 << "\n";
+
+
+    }
+}
+
