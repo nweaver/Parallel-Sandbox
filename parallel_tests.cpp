219aca63cbb776c2255bcdc577a9b7d278ec261d
Nicholas Weaver <ncweaver@ucdavis.edu>
2025-04-30 16:51:57 +0000

    (Cgekhpoint.

--- parallel_tests.cpp
+++ parallel_tests.cpp
@@ -1,12 +1,18 @@
 #include <gtest/gtest.h>
 
-
+#include <omp.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(ParallelTest, HelloWorld)
+{
+    std::cout << "Hello from process: " << omp_get_thread_num() << "\n";
+#pragma omp parallel
+    {
+        std::cout << "Hello from process: " << omp_get_thread_num() << "\n";
+    }
+    std::cout << "Hello from process: " << omp_get_thread_num() << "\n";
 }
 
+TEST(ParallelTest, CacheNuke)
+{
+}
